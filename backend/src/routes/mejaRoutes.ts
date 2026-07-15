2a0be22bce2982c40a8de77d277ae71d6b1353f6
yurisikrar <114625944+yurisikrar@users.noreply.github.com>
2026-07-15 16:01:14 +0700

    .

--- backend/src/routes/mejaRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getSemuaMeja, kosongkanMeja } from "../controllers/mejaController";
-
-const router = Router();
-
-router.get("/", getSemuaMeja);
-router.put("/:id/kosongkan", kosongkanMeja);
-
-export default router;
